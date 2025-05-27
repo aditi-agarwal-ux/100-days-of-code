b7f8be90b8e5ca3397f3c9ed3a31ac5b354577ae
aditi agarwal <aditinov2000@gmail.com>
2025-05-27 16:48:44 +0530

    add changes

--- array_creation.cpp
+++ array_creation.cpp
@@ -4,7 +4,7 @@ int main(){
  int arr[]={1,2,3,4,5};//creation without defining about the size
  int nums[5]={10,20,30,40,50};//creation with defining about the size
  //here the size is got fixed at the compile time we cant fix it at the run time
-int n1=sizeof(arr)/sizeof(int);
+int n1=sizeof(arr)/sizeof(int);//method of calculating the actual size of array
 int n2=sizeof(nums)/sizeof(int);
 for(int i=0;i<n1;i++){
     cout<<arr[i]<<" ";
